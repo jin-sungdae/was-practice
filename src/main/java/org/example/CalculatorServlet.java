a3bfcaacfadf78ad85222db12126dcaf74686230
sjin <wlstjdeo00@gmail.com>
2023-01-30 07:58:43 +0900

    FEATURE : servlet service request 시 calculator calculate 실행

--- src/main/java/org/example/CalculatorServlet.java
+++ src/main/java/org/example/CalculatorServlet.java
@@ -1,11 +1,14 @@
 package org.example;
 
+import org.example.calculate.Calculator;
+import org.example.calculate.PositiveNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/calculate")
 public class CalculatorServlet implements Servlet {
@@ -19,8 +22,16 @@ public class CalculatorServlet implements Servlet {
     }
 
     @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
         log.info("service");
+        int operand1 = Integer.parseInt(request.getParameter("operand1"));
+        String operator = request.getParameter("operator");
+        int operand2 = Integer.parseInt(request.getParameter("operand2"));
+
+        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+
+        PrintWriter writer = response.getWriter();
+        writer.println(result);;
     }
 
     @Override
