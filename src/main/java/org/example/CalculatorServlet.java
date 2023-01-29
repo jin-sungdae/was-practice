4969065f913b9289264de57327559e77692b1c09
sjin <wlstjdeo00@gmail.com>
2023-01-30 08:02:28 +0900

    FEATURE : servlet -> GenericServlet으로 변경 service 만 구현해도 됌

--- src/main/java/org/example/CalculatorServlet.java
+++ src/main/java/org/example/CalculatorServlet.java
@@ -11,15 +11,9 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet("/calculate")
-public class CalculatorServlet implements Servlet {
+public class CalculatorServlet extends GenericServlet {
 
     private static final Logger log = LoggerFactory.getLogger(CalculatorServlet.class);
-
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        log.info("init");
-
-    }
 
     @Override
     public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
@@ -32,21 +26,6 @@ public class CalculatorServlet implements Servlet {
 
         PrintWriter writer = response.getWriter();
         writer.println(result);;
-    }
-
-    @Override
-    public void destroy() {
-
-    }
-
-    @Override
-    public String getServletInfo() {
-        return null;
-    }
-
-    @Override
-    public ServletConfig getServletConfig() {
-        return null;
     }
 
 
