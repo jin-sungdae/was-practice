1c7905dc429d458b705dc6c40052fc2fb37c2479
sjin <wlstjdeo00@gmail.com>
2023-01-30 07:47:32 +0900

    FEATURE : Tommcat gradle 추가, tomcat 구성

---
+++ src/main/java/org/example/CalculatorServlet.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class CalculatorServlet implements Servlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
