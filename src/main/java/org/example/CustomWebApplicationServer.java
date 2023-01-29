c8e738d8f82bd6e204a95db2bcec1b8f819d87cd
sjin <wlstjdeo00@gmail.com>
2023-01-30 07:17:35 +0900

    FEATURE : Step3 Thread Pool을 적용해 안정적인 서비스가 가능하다록 변경

--- src/main/java/org/example/CustomWebApplicationServer.java
+++ src/main/java/org/example/CustomWebApplicationServer.java
@@ -9,10 +9,13 @@ import org.slf4j.LoggerFactory;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CustomWebApplicationServer {
     private final int port;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
 
@@ -30,7 +33,11 @@ public class CustomWebApplicationServer {
             while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("[CustomWebApplicationServer] client connected!");
 
-                new Thread(new ClientRequestHandler(clientSocket)).start();
+                /**
+                 * Step3 - Thread Pool을 적용해 안정적인 서비스가 가능하도록 한다.
+                 */
+
+                executorService.execute(new ClientRequestHandler(clientSocket));
             }
         }
     }
