29afde9857ba19eb1fdb8f44b17542934972bd39
sjin <wlstjdeo00@gmail.com>
2023-01-30 00:41:21 +0900

    SET : log, test gradle 추가

---
+++ src/main/java/org/example/CustomWebApplicationServer.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class CustomWebApplicationServer {
+    private final int port;
+
+    private static final Logger logger = LoogerFactory.getLogger(CustomWebApplicationServer.class);
+
+    public CustomWebApplicationServer(int port) {
+        this.port = port;
+    }
+
+    public void start() {
+
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+// GET /calculate?operand1=11&operator=*operand2=55
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
